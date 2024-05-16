2f849d1c0c1254f07f160a0d54438f4a93bab2de
Saito-3815 <sai.engineer3815@gmail.com>
2024-05-17 00:05:04 +0900

    ESlint setup #22

--- frontend/app/src/apis/users.js
+++ frontend/app/src/apis/users.js
@@ -1,32 +1,27 @@
 import axios from 'axios';
 
-export const fetchUser =(accessToken) => {
-  return axios.get("https://api.spotify.com/v1/me", {
-    headers: { 
-      Authorization: `Bearer ${accessToken}`, 
-    }
-  })
-  .then(response => {
-    return response.data //返り値をresという名前で取得し、res.dataでレスポンスの中身だけをreturn
-  })
+export const fetchUser = (accessToken) => axios.get('https://api.spotify.com/v1/me', {
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+  },
+})
+  .then((response) => response.data, // 返り値をresという名前で取得し、res.dataでレスポンスの中身だけをreturn
+  )
   .catch((error) => {
     if (error.response) {
       // サーバーからのエラーレスポンス
-      console.error("エラーレスポンスデータ:", error.response.data);
-      console.error("ステータスコード:", error.response.status);
-      console.error("ヘッダー:", error.response.headers);
+      console.error('エラーレスポンスデータ:', error.response.data);
+      console.error('ステータスコード:', error.response.status);
+      console.error('ヘッダー:', error.response.headers);
     } else if (error.request) {
       // リクエストが行われましたが、応答がありませんでした
-      console.error("リクエストが行われましたが、応答がありませんでした");
+      console.error('リクエストが行われましたが、応答がありませんでした');
     } else {
       // エラーをトリガーしたリクエストの設定中に何かが発生しました
-      console.error("エラー:", error.message);
+      console.error('エラー:', error.message);
     }
-    console.error("設定:", error.config);
+    console.error('設定:', error.config);
   });
-
-}
-
 
 //   .catch((e) => console.error(e))//console.error()でエラーメッセージをコンソールに出す
 // }
