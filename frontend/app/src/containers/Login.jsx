16b64aea59b4f46fda7eeb77bdca58e09c632b4f
Saito-3815 <sai.engineer3815@gmail.com>
2024-05-20 11:48:50 +0900

    mod #22

--- frontend/app/src/containers/Login.jsx
+++ frontend/app/src/containers/Login.jsx
@@ -5,7 +5,7 @@ function Login() {
   return (
     <div className="Login">
       <h2>ログイン前です</h2>
-      <a href={accessUrl}>spotifyへログイン</a>
+      <a href={accessUrl}>spotifyログイン</a>
     </div>
   );
 }
