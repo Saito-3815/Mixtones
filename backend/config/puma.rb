668198edc36ddbd352ab67d8391bc40272ca609a
Saito-3815 <sai.engineer3815@gmail.com>
2024-07-15 18:44:08 +0900

    config update #37

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -15,10 +15,7 @@ worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-# port ENV.fetch('PORT') { 3000 }
-
-# ec2で起動用に追加
-bind "unix:///var/www/mixify/backend/tmp/sockets/puma.sock"
+port ENV.fetch('PORT') { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
