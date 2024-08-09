dd9b241da7fc71daf2cfa8a0957b8c331167b4e9
Saito-3815 <sai.engineer3815@gmail.com>
2024-08-09 17:46:05 +0900

    name change button to Button

--- frontend/app/src/containers/PassSignup.jsx
+++ frontend/app/src/containers/PassSignup.jsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { Button } from "@/components/ui/Button/Button";
+// import { Button } from "@/components/ui/Button/Button.jsx";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAtom } from "jotai";
 import { loginUser, userAtom } from "@/atoms/userAtoms";
@@ -7,6 +7,7 @@ import { useMutation } from "@tanstack/react-query";
 import { createPasswordUser } from "@/api/usersCreatePassword";
 import { Switch } from "@/components/ui/Switch/Switch";
 import { useState } from "react";
+import { Button } from "@/components/ui/Button/Button";
 // import { useEffect } from "react";
 
 const PassSignup = () => {
