388e0354032b24798d98e2b88adb51f160370168
Saito-3815 <sai.engineer3815@gmail.com>
2024-08-16 23:29:17 +0900

    Rename button.jsx to Button.jsx

---
+++ frontend/app/src/components/ui/Avatar/Avatar.jsx
@@ -0,0 +1,80 @@
+import * as React from "react";
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+import { cn } from "@/lib/utils";
+
+import PropTypes from "prop-types";
+
+const Avatar = React.forwardRef(({ className, size, ...props }, ref) => (
+  <AvatarPrimitive.Root
+    ref={ref}
+    className={cn(
+      `relative flex h-${size} w-${size} shrink-0 overflow-hidden rounded-full`,
+      className,
+    )}
+    {...props}
+  />
+));
+Avatar.displayName = AvatarPrimitive.Root.displayName;
+Avatar.propTypes = {
+  size: PropTypes.string,
+};
+
+const AvatarImage = React.forwardRef(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Image
+    ref={ref}
+    className={cn("aspect-square h-full w-full", className)}
+    {...props}
+  />
+));
+AvatarImage.displayName = AvatarPrimitive.Image.displayName;
+
+const AvatarFallback = React.forwardRef(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Fallback
+    ref={ref}
+    className={cn(
+      "flex h-full w-full items-center justify-center rounded-full bg-muted",
+      className,
+    )}
+    {...props}
+  />
+));
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
+
+Avatar.propTypes = {
+  className: PropTypes.string,
+};
+
+AvatarImage.propTypes = {
+  className: PropTypes.string,
+};
+
+AvatarFallback.propTypes = {
+  className: PropTypes.string,
+};
+
+AvatarSet.propTypes = {
+  src: PropTypes.string.isRequired,
+  size: PropTypes.string,
+};
+
+export { Avatar, AvatarImage, AvatarFallback };
+
+export function AvatarSet({ src, size }) {
+  const iconSize = size * 3;
+  return (
+    <Avatar size={size}>
+      <AvatarImage src={src} alt="avatar" />
+      <AvatarFallback className="bg-gray-300">
+        <FontAwesomeIcon
+          icon={faUser}
+          style={{ height: `${iconSize}px`, width: `${iconSize}px` }}
+          className="text-gray-500"
+        />
+      </AvatarFallback>
+    </Avatar>
+  );
+}
