7e126a99e6f5c23cc4ee8a12e6c746929947c476
Saito-3815 <sai.engineer3815@gmail.com>
2024-08-20 13:13:41 +0900

    modal auto scroll setup #15

--- frontend/app/src/components/ui/CommentModal/CommentModal.jsx
+++ frontend/app/src/components/ui/CommentModal/CommentModal.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useCallback } from "react";
 import Modal from "react-modal";
 import PropTypes from "prop-types";
 import { useComments } from "@/hooks/useComments";
@@ -21,8 +21,8 @@ const CommentModal = ({ isOpen, onRequestClose, communityId, tuneId }) => {
       borderRadius: "10px",
       width: "30%",
       height: "60%",
-      display: "flex", 
-      flexDirection: "column", 
+      display: "flex",
+      flexDirection: "column",
     },
     overlay: {
       backgroundColor: "rgba(0, 0, 0, 0.5)",
@@ -37,17 +37,17 @@ const CommentModal = ({ isOpen, onRequestClose, communityId, tuneId }) => {
 
   const comments = Array.isArray(commentsData) ? commentsData : [];
 
-  const modalContentRef = useRef(null);
+  // モーダルを開いた時にスクロールを最下部へ
+  const scrollBottom = useCallback((node) => {
+    // 引数にnodeを受け取る
+    if (!node) return; // nodeがnullの場合はリターンして処理終了
+    node.scrollTop = node.scrollHeight;
+  }, [commentsData]);
 
   // モーダルを開いた時にコメントを取得
   const handleAfterOpen = () => {
-    if (modalContentRef.current) {
-      modalContentRef.current.scrollTop = modalContentRef.current.scrollHeight;
-    }
     refetchComments();
   };
-
-  // const scroll = useRef();
 
   return (
     <Modal
@@ -56,38 +56,36 @@ const CommentModal = ({ isOpen, onRequestClose, communityId, tuneId }) => {
       style={commentStyles}
       onAfterOpen={handleAfterOpen}
     >
-       <div className="flex-grow overflow-y-auto p-3"> 
-      {commentsError ? (
-        <p className="text-red-500">
-          エラーが発生しました: {commentsError.message}
-        </p>
-      ) : comments.length > 0 ? (
-        comments.map((comment) => (
-          <div key={comment.id} className="text-white">
-            <p>{comment.body}</p>
-            <p>by {comment.user.name}</p>
-            <p>
-              at {format(new Date(comment.created_at), "yyyy-MM-dd HH:mm:ss")}
-            </p>
-            <img
-              src={comment.user.avatar}
-              alt="images"
-              className="object-cover h-10 w-10 rounded-sm flex-shrink-0"
-            />
-          </div>
-        ))
-      ) : (
-        <p className="text-white">コメント機能は現在開発中です</p>
-      )}
+      <div className="flex-grow overflow-y-auto p-3" ref={scrollBottom}>
+        {commentsError ? (
+          <p className="text-red-500">
+            エラーが発生しました: {commentsError.message}
+          </p>
+        ) : comments.length > 0 ? (
+          comments.map((comment) => (
+            <div key={comment.id} className="text-white flex p-[20px_10px_0_20px] m-[20px] rounded-[3000px] shadow-[0_0_10px_rgb(164,164,164)] items-center">
+              <p>{comment.body}</p>
+              <p>by {comment.user.name}</p>
+              <p>
+                at {format(new Date(comment.created_at), "yyyy-MM-dd HH:mm:ss")}
+              </p>
+              <img
+                src={comment.user.avatar}
+                alt="images"
+                className="rounded-full h-[45px] mt-[-20px] border-2 border-black"
+              />
+            </div>
+          ))
+        ) : (
+          <p className="text-white">コメント機能は現在開発中です</p>
+        )}
       </div>
       <div className="bg-gray-100">
-          <SendComment
-            communityId={communityId}
-            tuneId={tuneId}
-            // scroll={scroll}
-          />
-          {/* <div ref={scroll}></div> */}
-        </div>
+        <SendComment
+          communityId={communityId}
+          tuneId={tuneId}
+        />
+      </div>
     </Modal>
   );
 };
--- frontend/app/src/components/ui/SendComment/SendComment.jsx
+++ frontend/app/src/components/ui/SendComment/SendComment.jsx
@@ -7,16 +7,14 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createComments } from "@/api/commentsCreate";
 import { Input } from "@mui/material";
 
-const SendComment = ({ communityId, tuneId}) => {
+const SendComment = ({ communityId, tuneId }) => {
   const [user] = useAtom(userAtom);
   const userId = user.id;
 
-
   const {
     register,
-    // setValue,
     handleSubmit,
-    // formState: { errors },
+    reset,
   } = useForm({ mode: "onChange" });
 
   const onSubmit = (data) => {
@@ -34,6 +32,7 @@ const SendComment = ({ communityId, tuneId}) => {
     onSuccess: (data) => {
       if (data.status === 201) {
         queryClient.setQueryData(["comment", communityId, tuneId], data.data);
+        reset();
       }
     },
     onError: (error) => {
@@ -44,17 +43,17 @@ const SendComment = ({ communityId, tuneId}) => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="flex items-center justify-center w-full border-t border-gray-300 ml-[-5px] p-2 pb-5 bg-gray-100">
-      <Input
-        type="text"
-        id="comment"
-        className="p-1 w-5/6 text-base font-semibold ml-1 mb-[-3px]"
-        placeholder="コメントを入力してください"
-        {...register("comment", {})}
+        <Input
+          type="text"
+          id="comment"
+          className="p-1 w-5/6 text-base font-semibold ml-1 mb-[-3px]"
+          placeholder="コメントを入力してください"
+          {...register("comment", {})}
         />
-      <button type="submit" className="bg-theme-orange text-white p-1 ml-2">
-        <SendIcon size={24} />
-      </button>
-        </div>
+        <button type="submit" className="bg-theme-orange text-white p-1 ml-2">
+          <SendIcon size={24} />
+        </button>
+      </div>
     </form>
   );
 };
